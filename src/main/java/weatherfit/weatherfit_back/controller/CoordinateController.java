e6efbcfbcbdc03312147b2c9172d21bc1e3bb807
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-02-28 15:25:29 +0900

    coordinate api update

--- src/main/java/weatherfit/weatherfit_back/controller/CoordinateController.java
+++ src/main/java/weatherfit/weatherfit_back/controller/CoordinateController.java
@@ -21,13 +21,66 @@ public class CoordinateController {
     private final CoordinateService coordinateService;
 
 
-    //착장 정보 조회
+    //착장 정보 한번에 조회
     @GetMapping("/list")
     public ResponseEntity<List<Coordinate>> getCoordinateList() {
         List<Coordinate> coordinateList = coordinateService.getCoordinateList();
         return ResponseEntity.ok(coordinateList);
     }
 
+    ///////////////////
 
+    // 착장 정보 중 preference 컬럼 조회
+    @GetMapping("/preference")
+    public ResponseEntity<List<String>> getPreferenceList() {
+        List<String> preferenceList = coordinateService.getPreferenceList();
+        return ResponseEntity.ok(preferenceList);
+    }
+ 
+    // 착장 정보 중 preference 컬럼 중 미니멀만 조회.    
+    @GetMapping("/preference/minimal")
+    public ResponseEntity<List<String>> getMinimalPreferenceList() {
+        List<String> minimalPreferenceList = coordinateService.getMinimalPreferenceList();
+        return ResponseEntity.ok(minimalPreferenceList);
+    }
+
+    // 착장 정보 중 preference 컬럼 중 모던만 조회.    
+    @GetMapping("/preference/modern")
+    public ResponseEntity<List<String>> getModernPreferenceList() {
+        List<String> modernPreferenceList = coordinateService.getModernPreferenceList();
+        return ResponseEntity.ok(modernPreferenceList);
+    }
+
+    // 착장 정보 중 preference 컬럼 중 캐주얼만 조회.    
+    @GetMapping("/preference/casual")
+    public ResponseEntity<List<String>> getCasualPreferenceList() {
+        List<String> casualPreferenceList = coordinateService.getCasualPreferenceList();
+        return ResponseEntity.ok(casualPreferenceList);
+    }
+
+    // 착장 정보 중 preference 컬럼 중 스트릿만 조회.    
+    @GetMapping("/preference/street")
+    public ResponseEntity<List<String>> getStreetPreferenceList() {
+        List<String> streetPreferenceList = coordinateService.getStreetPreferenceList();
+        return ResponseEntity.ok(streetPreferenceList);
+    }
+
+    // 착장 정보 중 preference 컬럼 중 러블리만 조회.    
+    @GetMapping("/preference/lively")
+    public ResponseEntity<List<String>> getLivelyPreferenceList() {
+        List<String> livelyPreferenceList = coordinateService.getLivelyPreferenceList();
+        return ResponseEntity.ok(livelyPreferenceList);
+    }
+    
+    //착장 정보 중 preference 컬럼 중 럭셔리만 조회.    
+    @GetMapping("/preference/luxury")
+    public ResponseEntity<List<String>> getLuxuryPreferenceList() {
+        List<String> luxuryPreferenceList = coordinateService.getLuxuryPreferenceList();
+        return ResponseEntity.ok(luxuryPreferenceList);
+    }
+    
+    
+    
+    
     
 }
--- src/main/java/weatherfit/weatherfit_back/service/CoordinateService.java
+++ src/main/java/weatherfit/weatherfit_back/service/CoordinateService.java
@@ -9,7 +9,7 @@ import weatherfit.weatherfit_back.repository.CoordinateRepository;
 import weatherfit.weatherfit_back.entity.User;
 import weatherfit.weatherfit_back.repository.LikeRepository;
 import weatherfit.weatherfit_back.repository.UserRepository;
-
+import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -22,6 +22,61 @@ public class CoordinateService {
     //착장 정보 조회
     public List<Coordinate> getCoordinateList() {
         return coordinateRepository.findAll();
+    }
+
+    //착장 정보 중 preference 컬럼 조회
+    public List<String> getPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 미니멀만 조회.    
+    public List<String> getMinimalPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("미니멀"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 모던만 조회.    
+    public List<String> getModernPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("모던"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 캐주얼만 조회.    
+    public List<String> getCasualPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("캐주얼"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 스트릿만 조회.    
+    public List<String> getStreetPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("스트릿"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 러블리만 조회.    
+    public List<String> getLivelyPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("러블리"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
+    }
+
+    //착장 정보 중 preference 컬럼 중 럭셔리만 조회.    
+    public List<String> getLuxuryPreferenceList() {
+        return coordinateRepository.findAll().stream()
+            .filter(coordinate -> coordinate.getPreference().equals("럭셔리"))
+            .map(Coordinate::getPreference)
+            .collect(Collectors.toList());
     }
 
     //착장 좋아요 여부 확인
